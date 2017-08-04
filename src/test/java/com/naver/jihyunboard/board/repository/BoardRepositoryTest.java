9b288398f686c473909c5e01426126b499f09caa
JihyunYun <ji.hyun.yun@navercorp.com>
2017-08-04 19:56:06 +0900

    윤지현 테스트

--- src/test/java/com/naver/jihyunboard/board/repository/BoardRepositoryTest.java
+++ src/test/java/com/naver/jihyunboard/board/repository/BoardRepositoryTest.java
@@ -1,11 +1,6 @@
 package com.naver.jihyunboard.board.repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,21 +8,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath*:application-context.xml")
 public class BoardRepositoryTest {
 
-	@Autowired
-	BoardRepository boardDao;
-
-	@Test
-	public void test() throws Exception {
-		Map<String, String> map = new HashMap<>();
-		map.put("searchOption", "all");
-		map.put("keyword", "복학");
-
-		/*	int count = boardDao.listCount(map);
-			assertNotSame(0, count);
-		*/
-		//		Map<String, Object> map = new HashMap<>();
-		//		boardDao.listAll(map);
-
-	}
+	/*	@Autowired
+		BoardRepository boardDao;
+	
+		@Test
+		public void test() throws Exception {
+			Map<String, String> map = new HashMap<>();
+			map.put("searchOption", "all");
+			map.put("keyword", "복학");
+	
+				int count = boardDao.listCount(map);
+				assertNotSame(0, count);
+			
+			//		Map<String, Object> map = new HashMap<>();
+			//		boardDao.listAll(map);
+	
+		}*/
 
 }
